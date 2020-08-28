90fdc0fdd7dc91880b6a3044091991fdb62736d8
Vukan Mrvaljevic <vukanmrvaljevic@vukans-mbp.home>
2020-08-27 21:29:36 -0400

    fixing home page css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css"
 import Actors from "./FilmDirectors/Actors/Actors"
 import Ideas from "./FilmDirectors/Ideas/Ideas"
 import Notes from "./FilmDirectors/Notes/Notes"
-import Menu from "./Menu"
+import Menu from "./Menu/Menu"
 import Artifacts from "./Historians/Artifacts"
 import Story from "./Literature/Story"
 
